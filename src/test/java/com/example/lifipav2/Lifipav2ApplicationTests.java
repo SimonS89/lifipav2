4b737d75cca00a0e183d5a8e8641f6c562da6623
SimonS89 <tripero89lp@gmail.com>
2023-03-20 19:51:19 -0300

    first commit

---
+++ src/test/java/com/example/lifipav2/Lifipav2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifipav2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lifipav2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
